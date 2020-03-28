845dfa46e8879f8d7548800d1a6031e053560b27
Derick Bui <derickbui64@gmail.com>
2020-03-28 15:17:24 -0700

    Update Commit

--- romcom/src/romcom/Main.java
+++ romcom/src/romcom/Main.java
@@ -16,21 +16,25 @@ public class Main {
       int drones = Integer.parseInt(readFile.next());
       int search[][] = new int[drones + 1][days + 1];
       int number = 0;
-
+      
+      boolean flag = true;
+      
       if (days == 0) {
         System.out.println(0);
         outputFile.println(0);
       }
       else if (days == 1) {
-        System.out.println(1);
-        outputFile.println(1);
+    	  flag = false;
+    	  System.out.println(1);
+    	  outputFile.println(1);
       }
       else if (drones == 1) {
+    	  flag = false;
     	  System.out.println(days);
     	  outputFile.println(days);
       }
       else {
-        for (int i = 1; i <= drones; i++) { //days
+        for (int i = 1; i <= drones; i++) {
           search[i][0] = 0;
           search[i][1] = 1;
         }
@@ -43,17 +47,28 @@ public class Main {
           for (int j = 2; j <= days; j++) {
             search[i][j] = Integer.MAX_VALUE;
             for (int k = 1; k <= j; k++) {
-              number = 1 + Math.max(search[i - 1][k - 1], search[i][j - k]);
-              if (search[i][j] > number) {
-                search[i][j] = number;
-              }
+            	int tempNum1 = search[i - 1][k - 1];
+            	int tempNum2 = search[i][j - k];
+            	
+            	if (tempNum1 > tempNum2) {
+            		number = 1 + tempNum1;
+            	}
+            	else {
+            		number = 1 + tempNum2;
+            	}
+            	
+            	if (search[i][j] > number) {
+            		search[i][j] = number;
+            	}
             }
           }
         }
       }
-
-      System.out.println(search[drones][days]);
-      outputFile.println(search[drones][days]);
+      
+      if (flag) {
+    	  System.out.println(search[drones][days]);
+          outputFile.println(search[drones][days]);  
+      }
 
       readFile.close();
       outputFile.close();
