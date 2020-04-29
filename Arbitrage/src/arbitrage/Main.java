5ad85d16f0531e3cdcacdc139ae20e2ca2b9e553
Derick Bui <derickbui64@gmail.com>
2020-04-29 00:17:32 -0700

    Initial Commit

--- Arbitrage/src/arbitrage/Main.java
+++ Arbitrage/src/arbitrage/Main.java
@@ -13,124 +13,147 @@ public class Main {
       File inputFile = new File("input.txt");
       Scanner readFile = new Scanner(inputFile);
 
-      int ndimension = Integer.parseInt(readFile.nextLine()) + 1 ;
+      int ndimension = Integer.parseInt(readFile.nextLine() + 1);
 
       double [][] grid = new double[ndimension][ndimension];
       ArrayList<Integer> path = new ArrayList<Integer>();
       double [] distance = new double[ndimension];
-      int [] p = new int[ndimension];
       double [] n = new double[ndimension];
       double valueSaver = 0;
 
       //read file and create adjacency matrix
-      for (int i = 1; i < grid.length; i++) {
-        for (int j = 1; j < grid[i].length; j++) {
-          grid[i][j] = Double.parseDouble(readFile.next());
-          grid[i][j] = -Math.log(grid[i][j]) / Math.log(2);
+//      for (int i = 1; i < grid.length; i++) {
+//        for (int j = 1; j < grid[i].length; j++) {
+//          grid[i][j] = Double.parseDouble(readFile.next());
+//          grid[i][j] = -Math.log(grid[i][j]) / Math.log(2);
+//        }
+//      }
+      
+    double testgrid[][] = new double[5][5];
+//    testgrid[1][1] = 0;
+//    testgrid[1][2] = -0.648613;
+//    testgrid[1][3] = -0.327216;
+//    testgrid[1][4] = 0.0305971;
+//    testgrid[2][1] = 0.648614;
+//    testgrid[2][2] = 0;
+//    testgrid[2][3] = 0.321387;
+//    testgrid[2][4] = 0.67922;
+//    testgrid[3][1] = 0.327227;
+//    testgrid[3][2] = -0.321386;
+//    testgrid[3][3] = 0;
+//    testgrid[3][4] = 0.357833;
+//    testgrid[4][1] = -0.0305903;
+//    testgrid[4][2] = -0.679217;
+//    testgrid[4][3] = -0.357822;
+//    testgrid[4][4] = 0;
+    
+    testgrid[1][1] = 1;
+    testgrid[1][2] = 1.56766;
+    testgrid[1][3] = 1.25459;
+    testgrid[1][4] = 0.979015;
+    testgrid[2][1] = 0.637893;
+    testgrid[2][2] = 1;
+    testgrid[2][3] = 0.8003;
+    testgrid[2][4] = 0.624503;
+    testgrid[3][1] = 0.797067;
+    testgrid[3][2] = 1.24953;
+    testgrid[3][3] = 1;
+    testgrid[3][4] = 0.780336;
+    testgrid[4][1] = 1.02143;
+    testgrid[4][2] = 1.60127;
+    testgrid[4][3] = 1.28149;
+    testgrid[4][4] = 1;
+    
+    for (int i = 1; i < 5; i++) {
+    	for (int j = 1; j < 5; j++) {
+    		testgrid[i][j] = -Math.log(testgrid[i][j]) / Math.log(2);
+    	}
+    }
+    
+//    testgrid[1][1] = 0;
+//    testgrid[1][2] = 5;
+//    testgrid[1][3] = 9;
+//    testgrid[1][4] = Double.POSITIVE_INFINITY;
+//    testgrid[2][1] = Double.POSITIVE_INFINITY;
+//    testgrid[2][2] = 0;
+//    testgrid[2][3] = 1;
+//    testgrid[2][4] = Double.POSITIVE_INFINITY;
+//    testgrid[3][1] = Double.POSITIVE_INFINITY;
+//    testgrid[3][2] = Double.POSITIVE_INFINITY;
+//    testgrid[3][3] = 0;
+//    testgrid[3][4] = 2;
+//    testgrid[4][1] = Double.POSITIVE_INFINITY;
+//    testgrid[4][2] = 3;
+//    testgrid[4][3] = Double.POSITIVE_INFINITY;
+//    testgrid[4][4] = 0;
+    
+    ndimension = 5;
+    
+    double storevaluegrid[][][] = new double[ndimension][ndimension][ndimension];
+    int storepathgrid[][][] = new int[ndimension][ndimension][ndimension];
+    int pathgrid[][] = new int[ndimension][ndimension];
+    double wgrid[][] = new double[ndimension][ndimension];
+    double finalgrid[][] = new double[ndimension][ndimension];
+    
+    //Create w grid
+    for (int i = 1; i < ndimension; i++) {
+    	for (int j = 1; j < ndimension; j++) {
+    		wgrid[i][j] = testgrid[i][j];
+    	}
+    }
+    
+    //Min-Plus Matrix Multiplication Algorithm
+    for (int p = 1; p < ndimension; p++) {
+    	finalgrid = new double[ndimension][ndimension];
+    	
+    	for (int k = 1; k < ndimension; k++) {
+    		for (int l = 1; l < ndimension; l++) {
+    			finalgrid[k][l] = Double.POSITIVE_INFINITY;
+    		}
+    	}
+    	
+    	for (int i = 1; i < ndimension; i++) {
+        	for (int j = 1; j < ndimension; j++) {
+        		for (int x = 1; x < ndimension; x++) {
+        			if (finalgrid[i][j] >= (testgrid[i][x] + wgrid[x][j])) {
+          			  finalgrid[i][j] = testgrid[i][x] + wgrid[x][j];
+          			  pathgrid[i][j] = x;
+        			}
+        		}
+        	}
         }
-      }
+    	
+    	for (int k = 1; k < ndimension; k++) {
+    		for (int l = 1; l < ndimension; l++) {
+    			testgrid[k][l] = finalgrid[k][l];
+    			storevaluegrid[p][k][l] = finalgrid[k][l];
+    		}
+    	}
+    	
+    	for (int k = 1; k < ndimension; k++) {
+    		for (int l = 1; l < ndimension; l++) {
+    			storepathgrid[p][k][l] = pathgrid[k][l];
+    		}
+    	}
+    	
+    	for (int k = 1; k < ndimension; k++) {
+    		for (int l = 1; l < ndimension; l++) {
+    			System.out.print(storevaluegrid[p][k][l] + " ");
+    		}
+    		System.out.println("");
+    	}
+    	
+    	System.out.println("");
+    	
+    	for (int k = 1; k < ndimension; k++) {
+    		for (int l = 1; l < ndimension; l++) {
+    			System.out.print(storepathgrid[p][k][l] + " ");
+    		}
+    		System.out.println("");
+    	}
 
-      //normal APSP calculation, does not take arbitrage weight into account
-//      for (int k = 1; k < grid.length; k++) {
-//          for (int i = 1; i < grid.length; i++) {
-//        	  for (int j = 1; j < grid.length; j++) {
-//        		  if (grid[i][j] > (grid[i][k] + grid[k][j])) {
-//        			  grid[i][j] = grid[i][k] + grid[k][j];
-//        		  }
-//        	  }
-//          }
-//      }
-      
-      double testgrid[][] = new double[4][4];
-      testgrid[1][1] = 0;
-      testgrid[1][2] = 1.2;
-      testgrid[1][3] = .89;
-      testgrid[2][1] = .88;
-      testgrid[2][2] = 0;
-      testgrid[2][3] = .51;
-      testgrid[3][1] = 1.1;
-      testgrid[3][2] = 0.15;
-      testgrid[3][3] = 0;
-      
-      ndimension = 4;
-      
-      for (int step = 0; step < ndimension; step++) { //solving time
-    	  for (int i = 0; i < ndimension; i++) { //reset array to zero
-    		  distance[i] = 0;
-    	  }
-    	  
-    	  distance[step] = 1;
-    	  
-    	  for(int k = 1; k < ndimension; k++) {
-    		  for (int i = 1; i < ndimension; i++) {
-    			  n[i] = distance[i];
-    		  }
-    		  
-    		  for (int i = 1; i < ndimension; i++) {
-    			  if (distance[i] > 0) {
-    				  for (int j = 1; j < ndimension; j++) {
-    					  double temp = distance[i] * testgrid[i][j];
-    					  if (n[j] < temp) {
-    						  n[j] = temp;
-    						  p[j] = i;
-    					  }
-    				  }
-    			  }
-    		  }
-    		  
-    		  for (int i = 1; i < ndimension; i++) {
-    			  distance[i] = n[i];
-    		  }
-    		  
-    		  if (distance[step] > 1.01) {
-    			  ArrayList<Integer> tempArrayList = new ArrayList<Integer>();
-    			  for (int i = 1, tempStep = step; i <= k; i++) {
-    				  tempArrayList.add(tempStep);
-    				  tempStep = p[tempStep];
-    			  }
-    			  
-    			  //System.out.println(distance[step]);
-    			  System.out.println("Distance: " + distance[step]);
-    			  System.out.println("Temp size: " + tempArrayList.size() + 1);
-    			  double efficiency = Math.pow(distance[step], (1.0/(tempArrayList.size() + 1)));
-    			  System.out.println("Before rounding: " + efficiency);
-    			  efficiency = Math.pow(Math.round(efficiency), (tempArrayList.size() + 1));
-    			  System.out.println("After rounding: " + efficiency);
-    			  System.out.println(efficiency);
-    			  
-    			  if (path.isEmpty() || (valueSaver < efficiency)) { //needs checking
-    				  path = tempArrayList;
-    				  valueSaver = efficiency;
-    			  }
-    		  }
-    	  }
-    	  
-      }
-      
-	  if (path.isEmpty()) {
-		  System.out.println("Lol");
-	  }
-	  else {
-		  System.out.print(path.get(0) + " ");
-		  for (int i = path.size() - 1; i >= 0; i--) {
-			  System.out.print(path.get(i) + " ");
-		  }
-	  }
-      
-      System.out.println("");
-      
-      //prints weighted grid
-//      for (int i = 1; i < 5; i++) {
-//    	  for (int j = 1; j < 5; j++) {
-//    		  if (i == j) {
-//    			  System.out.print(grid[i][j] + " ");  
-//    		  }
-//    	  }
-//    	  System.out.println("");
-//      }
-       
-       
-      
+    	System.out.println("");
+    }
       
       readFile.close();
     }
